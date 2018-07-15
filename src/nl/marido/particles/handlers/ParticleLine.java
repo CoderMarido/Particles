3aa32d568f2a9ebca073e52fd81e6de5e9ef6f6c
montlikadani <montlikada@gmail.com>
2018-07-15 20:48:13 +0200

    Update ParticleLine.java

--- src/nl/marido/particles/handlers/ParticleLine.java
+++ src/nl/marido/particles/handlers/ParticleLine.java
@@ -14,44 +14,59 @@ public class ParticleLine {
 	private static Effect effect;
 	private static Sound sound;
 	private static Vector vector;
+	private static int volume = 0;
+	private static int pitch = 0.0;
+	private static int amount = 0;
 	private static int speed;
 	private static int distance;
 	private static boolean collide;
 
 	public ParticleLine(Location location) {
-		ParticleLine.location = location;
+		this.location = location;
 	}
 
-	public void setEffect(Effect effect) {
-		ParticleLine.effect = effect;
+	public static void setEffect(Effect effect) {
+		this.effect = effect;
 	}
 
-	public void setSound(Sound sound) {
-		ParticleLine.sound = sound;
+	public static void setAmount(int amount) {
+		this.amount = amount;
 	}
 
-	public void setVector(Vector vector) {
-		ParticleLine.vector = vector;
+	public static void setSound(Sound sound) {
+		this.sound = sound;
 	}
 
-	public void setSpeed(int speed) {
-		ParticleLine.speed = speed;
+	public static void setVector(Vector vector) {
+		this.vector = vector;
 	}
 
-	public void setDistance(int distance) {
-		ParticleLine.distance = distance;
+	public static void setSpeed(int speed) {
+		this.speed = speed;
 	}
 
-	public void setCollide(boolean collide) {
-		ParticleLine.collide = collide;
+	public static void setDistance(int distance) {
+		this.distance = distance;
 	}
 
-	public void spawnParticle() {
+	public static void setCollide(boolean collide) {
+		this.collide = collide;
+	}
+
+	public static void setVolume(int volume) {
+		this.volume = volume;
+	}
+
+	public static void setPitch(int pitch) {
+		this.pitch = pitch;
+	}
+
+	public static void spawnParticle() {
 		new BukkitRunnable() {
 			public void run() {
 				distance--;
-				location.getWorld().playEffect(location, effect, 50);
-				location.getWorld().playSound(location, sound, 1, 1);
+				location.getWorld().playEffect(location, effect, amount);
+				location.getWorld().playSound(location, sound, volume, pitch);
 				location = location.add(vector);
 				if ((location.getBlock().getType().isSolid() && collide) || distance <= 0) {
 					cancel();
